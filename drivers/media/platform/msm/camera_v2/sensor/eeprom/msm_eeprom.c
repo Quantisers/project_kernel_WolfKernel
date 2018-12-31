6f63ec9e04f48b699c58b8326c7318e8fdf12b5f
Ryan Andri <ryanandri@linuxmail.org>
2018-12-31 20:38:43 +0700

    :wolf: drivers: camera_v2: checkout to LA.UM.6.6.r1-10400-89xx.0
    * The latest camera driver break Gcam auto focus.
      Back to caf stack for oreo to make it working again.
    * This is not help for issue on Gcam 6+ recording.
    
    includes:
      182d690 msm: camera: Remove unnecessary parentheses
      7e15656 Revert "msm: camera: Bypass redundant creation of video node in stereo"
      1079b66 msm: camera_v2: selection Xiaomi kernel changes for Redmi 4/4X
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>
    Signed-off-by: Quantummech2000 <vvr-rockstar@bmsit.in>

--- drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
+++ drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2016, 2018 The Linux Foundation. All rights reserved.
  * Copyright (C) 2018 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
