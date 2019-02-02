c313816ce1be3b82613b038a86c273dd5156d6dd
Quantummech2000 <vvr-rockstar@bmsit.in>
2019-02-02 10:21:23 +0000

    :wolf: update script
    Kernel version present in Zip-Name from now on
    
    Signed-off-by: Quantummech2000 <vvr-rockstar@bmsit.in>

--- kernel-65Hz.sh
+++ kernel-65Hz.sh
@@ -72,7 +72,7 @@ export TCVERSION1="$(${CROSS_COMPILE}gcc --version | head -1 |\
 awk -F '(' '{print $2}' | awk '{print tolower($1)}')"
 export TCVERSION2="$(${CROSS_COMPILE}gcc --version | head -1 |\
 awk -F ')' '{print $2}' | awk '{print tolower($1)}')"
-export ZIPNAME="${KERNELNAME}-${DEVICE}-TREBLE-65Hz-$(date +%Y%m%d-%H%M).zip"
+export ZIPNAME="${KERNELNAME}-5.7-${DEVICE}-TREBLE-65Hz-$(date +%Y%m%d-%H%M).zip"
 export FINAL_ZIP="${ZIP_DIR}/${ZIPNAME}"
 
 [ ! -d "${ZIP_DIR}" ] && mkdir -pv ${ZIP_DIR}
